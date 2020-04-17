8e9ec97a0c2cbfcb0cf7cfe3fa716ab4a1a1e294
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-04-17 09:51:16 +0530

    Add Case Study in Home

--- src/components/home/caseStudy.jsx
+++ src/components/home/caseStudy.jsx
@@ -5,43 +5,47 @@ import "./../../styles/caseStudy.scss";
 export default class CaseStudy extends Component {
   render() {
     return (
-      <section className="case-study">
+      <section className="case-study section">
         <Heading
           subtitle="featured jamstack case study"
           title="JAMstack in Services of Better education: Avenues, the World School."
         />
-        <div>
-          <div>
-            <div>
-              <div>
+        <div className="case-study-content">
+          <div className="case-study-hero">
+            <div className="img-container">
+              <div className="img-wrapper">
                 <img src={avenue} alt="avenues case study" />
               </div>
             </div>
           </div>
           <div>
-            <div>
-              <img src={quote} alt="" />
-              <div>
-                For us, the benefits (of JAMstack) are straight forward. Cost is
-                an obvious win as well as the ease of implementing enhancements
-                and inherent security improvements.
+            <blockquote className="case-study-quote">
+              <div className="quote-img">
+                <img src={quote} alt="" />
               </div>
-              <div>
-                <div>
+              For us, the benefits (of JAMstack) are straight forward. Cost is
+              an obvious win as well as the ease of implementing enhancements
+              and inherent security improvements.
+              <div className="user">
+                <div className="user-img">
                   <img src={dmitri} alt="dmitri" />
                 </div>
-                <div>Dmitry Terner</div>
-                <div>Website Manager at Avenues</div>
+                <div className="user-info">
+                  <div className="user-name">Dmitry Terner</div>
+                  <div className="user-position">
+                    Website Manager at Avenues
+                  </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <div>
-                <div>781</div>
-                <div>static pages</div>
+            </blockquote>
+            <div className="stats">
+              <div className="stat-container">
+                <h3>781</h3>
+                <p>static pages</p>
               </div>
-              <div>
-                <div>80k</div>
-                <div>traffic, 0 downtime</div>
+              <div className="stat-container">
+                <h3>80k</h3>
+                <p>traffic, 0 downtime</p>
               </div>
             </div>
           </div>
@@ -51,8 +55,9 @@ export default class CaseStudy extends Component {
             <a href="/" className="cta">
               Read full case study
             </a>
+            <div className="divider"></div>
             <a href="/" className="read-more">
-              See 7 more case studies
+              See 7 more case studies »
             </a>
           </div>
         </footer>
