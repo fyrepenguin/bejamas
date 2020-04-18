6cc24ff7ea4369e6331501695a43ab686005a5f3
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-04-18 10:03:19 +0530

    Add stories

--- src/components/common/tech.jsx
+++ src/components/common/tech.jsx
@@ -30,39 +30,39 @@ export default class TechUsed extends Component {
         <div className="tool-types">
           <h3>Data Sources</h3>
           <div className="tool-images">
-            <img style={{ "max-width": "160px" }} src={contentful} alt="" />
+            <img style={{ maxWidth: "160px" }} src={contentful} alt="" />
 
-            <img style={{ "max-width": "160px" }} src={storyblok} alt="" />
+            <img style={{ maxWidth: "160px" }} src={storyblok} alt="" />
 
-            <img style={{ "max-width": "110px" }} src={sanity} alt="" />
-            <img style={{ "max-width": "160px" }} src={forestry} alt="" />
-            <img style={{ "max-width": "120px" }} src={dato} alt="" />
+            <img style={{ maxWidth: "110px" }} src={sanity} alt="" />
+            <img style={{ maxWidth: "160px" }} src={forestry} alt="" />
+            <img style={{ maxWidth: "120px" }} src={dato} alt="" />
           </div>
         </div>
         <div className="tool-types">
           <h3>Build</h3>
           <div className="tool-images">
-            <img style={{ "max-width": "160px" }} src={gatsby} alt="" />
+            <img style={{ maxWidth: "160px" }} src={gatsby} alt="" />
 
-            <img style={{ "max-width": "120px" }} src={next} alt="" />
+            <img style={{ maxWidth: "120px" }} src={next} alt="" />
 
-            <img style={{ "max-width": "150px" }} src={hugo} alt="" />
+            <img style={{ maxWidth: "150px" }} src={hugo} alt="" />
 
-            <img style={{ "max-width": "60px" }} src={react} alt="" />
+            <img style={{ maxWidth: "60px" }} src={react} alt="" />
 
-            <img style={{ "max-width": "120px" }} src={node} alt="" />
+            <img style={{ maxWidth: "120px" }} src={node} alt="" />
           </div>
         </div>
         <div className="tool-types">
           <h3>Deploy & CDN</h3>
           <div className="tool-images">
-            <img style={{ "max-width": "130px" }} src={netlify} alt="" />
+            <img style={{ maxWidth: "130px" }} src={netlify} alt="" />
 
-            <img style={{ "max-width": "50px" }} src={now} alt="" />
+            <img style={{ maxWidth: "50px" }} src={now} alt="" />
 
-            <img style={{ "max-width": "150px" }} src={firebase} alt="" />
+            <img style={{ maxWidth: "150px" }} src={firebase} alt="" />
 
-            <img style={{ "max-width": "70px" }} src={aws} alt="" />
+            <img style={{ maxWidth: "70px" }} src={aws} alt="" />
           </div>
         </div>
       </section>
