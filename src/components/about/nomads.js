fa59ce0451c580ebe08f983b0c35b49a9890548e
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-04-19 21:10:19 +0530

    Add Nomad Section structure

--- src/components/about/nomads.js
+++ src/components/about/nomads.js
@@ -3,11 +3,27 @@ import Heading from "./../common/heading";
 const Nomads = () => {
   return (
     <section className="section nomads">
-      <Heading
-        subtitle="digital nomads"
-        title="Behind the scenes of remote work"
-        description="Who said we have to work from one office to be a close-knit team?"
-      />
+      <div className="nomad-text">
+        <Heading
+          subtitle="digital nomads"
+          title="Behind the scenes of remote work"
+          description="Who said we have to work from one office to be a close-knit team?"
+        />
+        <div>
+          <p>
+            The mere fact we’re all different and have different lifestyles
+            gives us a chance to be more open and inspires us to make progress.
+            We don’t believe in the nine-to-five work routine, being tied up to
+            the desk, or forcing new employees to relocate.
+          </p>
+          <p>
+            We see the jobs of the future more our way - being ourselves, being
+            free, and living our dreams. There are things Slack can’t help with,
+            that’s why we regularly take trips, coming from different parts of
+            the world and having real fun.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
